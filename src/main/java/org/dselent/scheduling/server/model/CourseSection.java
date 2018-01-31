b06acf98349e565f87aeca3ec03f222834aac652
Robaire <robaire@galliath.com>
2018-01-31 14:56:39 -0500

    Created CourseSection model

---
+++ src/main/java/org/dselent/scheduling/server/model/CourseSection.java
@@ -0,0 +1,157 @@
+package org.dselent.scheduling.server.model;
+
+
+import javax.print.DocFlavor;
+import java.sql.JDBCType;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.*;
+
+
+public class CourseSection extends Model{
+
+    public static final String TABLE_NAME = "course_sections";
+
+    public static enum Columns{
+        ID,
+        COURSES_ID,
+        SECTION_TYPE,
+        CREATED_AT,
+        UPDATED_AT
+    }
+
+    // Make a List of the Columns
+    private static final List<Columns> COLUMN_LIST = new ArrayList<>();
+
+    // Type Mapping
+    private static final Map<Columns, JDBCType> COLUMN_TYPE_MAP = new HashMap<>();
+
+    static
+    {
+        for(Columns key : Columns.values())
+        {
+            COLUMN_LIST.add(key);
+        }
+
+        COLUMN_TYPE_MAP.put(Columns.ID, JDBCType.INTEGER);
+        COLUMN_TYPE_MAP.put(Columns.COURSES_ID, JDBCType.VARCHAR);
+        COLUMN_TYPE_MAP.put(Columns.SECTION_TYPE, JDBCType.VARCHAR);
+        COLUMN_TYPE_MAP.put(Columns.CREATED_AT, JDBCType.TIMESTAMP_WITH_TIMEZONE);
+        COLUMN_TYPE_MAP.put(Columns.UPDATED_AT, JDBCType.TIMESTAMP_WITH_TIMEZONE);
+    };
+
+    // Attributes
+    private Integer id;
+    private String coursesId;
+    private String sectionType;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    // Column Methods
+
+    public static JDBCType getColumnType(Columns column)
+    {
+        return COLUMN_TYPE_MAP.get(column);
+    }
+
+    public static String getColumnName(Columns column)
+    {
+        return column.toString().toLowerCase();
+    }
+
+    public static List<String> getColumnNameList()
+    {
+        List<String> columnNameList = new ArrayList<>();
+
+        for(Columns column : COLUMN_LIST)
+        {
+            columnNameList.add(getColumnName(column));
+        }
+
+        return columnNameList;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCoursesId() {
+        return coursesId;
+    }
+
+    public void setCoursesId(String coursesId) {
+        this.coursesId = coursesId;
+    }
+
+    public String getSectionType() {
+        return sectionType;
+    }
+
+    public void setSectionType(String sectionType) {
+        this.sectionType = sectionType;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt)
+    {
+        if(createdAt != null)
+        {
+            this.createdAt = createdAt.toInstant();
+        }
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt)
+    {
+        if(updatedAt != null)
+        {
+            this.updatedAt = updatedAt.toInstant();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CourseSection that = (CourseSection) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(coursesId, that.coursesId) &&
+                Objects.equals(sectionType, that.sectionType) &&
+                Objects.equals(createdAt, that.createdAt) &&
+                Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, coursesId, sectionType, createdAt, updatedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "CourseSection{" +
+                "id=" + id +
+                ", coursesId='" + coursesId + '\'' +
+                ", sectionType='" + sectionType + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
