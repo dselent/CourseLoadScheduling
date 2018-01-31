f4923bba7d362834228f3f960262c966eeaeb1ad
Robaire <robaire@galliath.com>
2018-01-31 17:13:04 -0500

    Created Department model

--- src/main/java/org/dselent/scheduling/server/model/Department.java
+++ src/main/java/org/dselent/scheduling/server/model/Department.java
@@ -6,4 +6,117 @@ import java.time.Instant;
 import java.util.*;
 
 public class Department extends Model {
+
+    public static final String TABLE_NAME = "departments";
+
+    public static enum Columns{
+        ID,
+        DEPARTMENT,
+        CREATED_AT,
+        UPDATED_AT
+    }
+
+    private static final List<Columns> COLUMN_LIST = new ArrayList<>();
+
+    public static final Map<Columns, JDBCType> COLUMN_TYPE_MAP = new HashMap<>();
+
+    static{
+        for(Columns key : Columns.values()){
+            COLUMN_LIST.add(key);
+        }
+
+        COLUMN_TYPE_MAP.put(Columns.ID, JDBCType.INTEGER);
+        COLUMN_TYPE_MAP.put(Columns.DEPARTMENT, JDBCType.VARCHAR);
+        COLUMN_TYPE_MAP.put(Columns.CREATED_AT, JDBCType.TIMESTAMP_WITH_TIMEZONE);
+        COLUMN_TYPE_MAP.put(Columns.UPDATED_AT, JDBCType.TIMESTAMP_WITH_TIMEZONE);
+    }
+
+    // Attributes
+    private Integer id;
+    private String department;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    // Column Methods
+    public static JDBCType getColumnType(Columns column)
+    {
+        return COLUMN_TYPE_MAP.get(column);
+    }
+
+    public static String getColumnName(Columns column)
+    {
+        return column.toString().toLowerCase();
+    }
+
+    public static List<String> getColumnNameList()
+    {
+        List<String> columnNameList = new ArrayList<>();
+
+        for(Columns column : COLUMN_LIST)
+        {
+            columnNameList.add(getColumnName(column));
+        }
+
+        return columnNameList;
+    }
+
+    // Setters and Getters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    // Function Overrides
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(department, that.department) &&
+                Objects.equals(createdAt, that.createdAt) &&
+                Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, department, createdAt, updatedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", department='" + department + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
