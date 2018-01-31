80e67e06f657a0d5b99666b135e0e0d9051045e8
Robaire <robaire@galliath.com>
2018-01-31 17:12:31 -0500

    Created CourseRequest model

---
+++ src/main/java/org/dselent/scheduling/server/model/Department.java
@@ -0,0 +1,9 @@
+package org.dselent.scheduling.server.model;
+
+import java.sql.JDBCType;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.*;
+
+public class Department extends Model {
+}
