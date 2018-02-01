ab856a58b8c46041d921059caaaf76fedda893d6
FabiGazi <fgaziano98@gmail.com>
2018-02-01 17:43:33 -0500

    Created CourseDao

---
+++ src/main/java/org/dselent/scheduling/server/dao/CourseDao.java
@@ -0,0 +1,11 @@
+package org.dselent.scheduling.server.dao;
+
+import org.dselent.scheduling.server.model.Course;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseDao extends Dao<Course>
+
+{
+    // add functions here as needed
+}
