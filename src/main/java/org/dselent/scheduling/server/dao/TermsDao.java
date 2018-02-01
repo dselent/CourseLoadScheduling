8c135111cd1645ebfad2be3c0e69e5a51a62e275
Robaire <robaire@galliath.com>
2018-02-01 17:18:11 -0500

    Created TermsDao

---
+++ src/main/java/org/dselent/scheduling/server/dao/TermsDao.java
@@ -0,0 +1,10 @@
+package org.dselent.scheduling.server.dao;
+
+import org.dselent.scheduling.server.model.Term;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TermsDao extends Dao<Term> {
+
+    // Add Functions
+}
