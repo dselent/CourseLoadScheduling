e9bd45f13ace3941d0ce4f528530e22ba4e5b713
NathanSiegel <siegelnathan08@gmail.com>
2018-02-01 18:43:50 -0500

    added course sections terms dao

--- src/main/java/org/dselent/scheduling/server/dao/CourseSectionsTermsDao.java
+++ src/main/java/org/dselent/scheduling/server/dao/CourseSectionsTermsDao.java
@@ -1,13 +1,14 @@
 package org.dselent.scheduling.server.dao;
 
-import org.dselent.scheduling.server.model.CourseSection;
+import org.dselent.scheduling.server.model.CourseSectionTerm;
 import org.springframework.stereotype.Repository;
+
 
 /**
  * Created by Nathan on 2/1/2018.
  */
 
 @Repository
-public interface CourseSectionsDao extends Dao<CourseSection>{
-
+public interface CourseSectionsTermsDao extends Dao<CourseSectionTerm>{
+    
 }
