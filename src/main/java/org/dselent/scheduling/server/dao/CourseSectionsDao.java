c21243b9b4d1240d2b03d2f9dd7d490e56be85b4
NathanSiegel <siegelnathan08@gmail.com>
2018-02-01 18:32:29 -0500

    course requests and sections interfaces

---
+++ src/main/java/org/dselent/scheduling/server/dao/CourseSectionsDao.java
@@ -0,0 +1,10 @@
+package org.dselent.scheduling.server.dao;
+
+import org.dselent.scheduling.server.model.CourseSection;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Nathan on 2/1/2018.
+ */
+public interface CourseSectionsDao extends Dao<CourseSection>{
+}
