016bee23849ae80f72ebe1cc69b149b299f8d308
NathanSiegel <siegelnathan08@gmail.com>
2018-02-02 12:51:13 -0500

    UsersPermissions Dao added

--- src/main/java/org/dselent/scheduling/server/dao/UsersPermissionsDao.java
+++ src/main/java/org/dselent/scheduling/server/dao/UsersPermissionsDao.java
@@ -1,11 +1,12 @@
 package org.dselent.scheduling.server.dao;
 
-import org.dselent.scheduling.server.model.FacultyDepartment;
+import org.dselent.scheduling.server.model.UserPermission;
 import org.springframework.stereotype.Repository;
+
 /**
  * Created by Nathan on 2/2/2018.
  */
 @Repository
-public interface FacultyDepartmentsDao extends Dao<FacultyDepartment> {
-
+public interface UsersPermissionsDao extends Dao<UserPermission> {
+    
 }
