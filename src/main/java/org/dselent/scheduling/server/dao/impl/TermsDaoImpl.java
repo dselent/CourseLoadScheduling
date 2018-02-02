1f3d571a599423bcff1e05291967d8032ea07b77
Michael Capobianco <mccapobianco@wpi.edu>
2018-02-02 12:13:51 -0500

    completed TermsDaoImpl

--- src/main/java/org/dselent/scheduling/server/dao/impl/TermsDaoImpl.java
+++ src/main/java/org/dselent/scheduling/server/dao/impl/TermsDaoImpl.java
@@ -1,6 +1,225 @@
 package org.dselent.scheduling.server.dao.impl;
 
-public class TermsDaoImpl
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.dselent.scheduling.server.dao.TermsDao;
+import org.dselent.scheduling.server.extractor.TermsExtractor;
+import org.dselent.scheduling.server.miscellaneous.Pair;
+import org.dselent.scheduling.server.miscellaneous.QueryStringBuilder;
+import org.dselent.scheduling.server.model.Term;
+import org.dselent.scheduling.server.sqlutils.ColumnOrder;
+import org.dselent.scheduling.server.sqlutils.ComparisonOperator;
+import org.dselent.scheduling.server.sqlutils.QueryTerm;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+
+/*
+ * @Repository annotation
+ * https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/stereotype/Repository.html
+ *
+ * Useful link
+ * https://howtodoinjava.com/spring/spring-core/how-to-use-spring-component-repository-service-and-controller-annotations/
+ */
+@Repository
+public abstract class TermsDaoImpl extends BaseDaoImpl<Term> implements TermsDao
 {
-    
+    @Override
+    public int insert(Term termModel, List<String> insertColumnNameList, List<String> keyHolderColumnNameList) throws SQLException
+    {
+
+        validateColumnNames(insertColumnNameList);
+        validateColumnNames(keyHolderColumnNameList);
+
+        String queryTemplate = QueryStringBuilder.generateInsertString(Term.TABLE_NAME, insertColumnNameList);
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+
+        List<Map<String, Object>> keyList = new ArrayList<>();
+        KeyHolder keyHolder = new GeneratedKeyHolder(keyList);
+
+        for(String insertColumnName : insertColumnNameList)
+        {
+            addParameterMapValue(parameters, insertColumnName, termModel);
+        }
+        // new way, but unfortunately unnecessary class creation is slow and wasteful (i.e. wrong)
+        // insertColumnNames.forEach(insertColumnName -> addParameterMap(parameters, insertColumnName, userModel));
+
+        int rowsAffected = namedParameterJdbcTemplate.update(queryTemplate, parameters, keyHolder);
+
+        Map<String, Object> keyMap = keyHolder.getKeys();
+
+        for(String keyHolderColumnName : keyHolderColumnNameList)
+        {
+            addObjectValue(keyMap, keyHolderColumnName, termModel);
+        }
+
+        return rowsAffected;
+
+    }
+
+
+    @Override
+    public List<Term> select(List<String> selectColumnNameList, List<QueryTerm> queryTermList, List<Pair<String, ColumnOrder>> orderByList) throws SQLException
+    {
+        TermsExtractor extractor = new TermsExtractor();
+        String queryTemplate = QueryStringBuilder.generateSelectString(Term.TABLE_NAME, selectColumnNameList, queryTermList, orderByList);
+
+        List<Object> objectList = new ArrayList<Object>();
+
+        for(QueryTerm queryTerm : queryTermList)
+        {
+            objectList.add(queryTerm.getValue());
+        }
+
+        Object[] parameters = objectList.toArray();
+
+        List<Term> termsList = jdbcTemplate.query(queryTemplate, extractor, parameters);
+
+        return termsList;
+    }
+
+    @Override
+    public Term findById(int id) throws SQLException
+    {
+        String columnName = QueryStringBuilder.convertColumnName(Term.getColumnName(Term.Columns.ID), false);
+        List<String> selectColumnNames = Term.getColumnNameList();
+
+        List<QueryTerm> queryTermList = new ArrayList<>();
+        QueryTerm idTerm = new QueryTerm(columnName, ComparisonOperator.EQUAL, id, null);
+        queryTermList.add(idTerm);
+
+        List<Pair<String, ColumnOrder>> orderByList = new ArrayList<>();
+        Pair<String, ColumnOrder> order = new Pair<String, ColumnOrder>(columnName, ColumnOrder.ASC);
+        orderByList.add(order);
+
+        List<Term> termsList = select(selectColumnNames, queryTermList, orderByList);
+
+        Term term = null;
+
+        if(!termsList.isEmpty())
+        {
+            term = termsList.get(0);
+        }
+
+        return term;
+    }
+
+    @Override
+    public int update(String columnName, Object newValue, List<QueryTerm> queryTermList)
+    {
+        String queryTemplate = QueryStringBuilder.generateUpdateString(Term.TABLE_NAME, columnName, queryTermList);
+
+        List<Object> objectList = new ArrayList<Object>();
+        objectList.add(newValue);
+
+        for(QueryTerm queryTerm : queryTermList)
+        {
+            objectList.add(queryTerm.getValue());
+        }
+
+        Object[] parameters = objectList.toArray();
+
+        int rowsAffected = jdbcTemplate.update(queryTemplate, parameters);
+
+        return rowsAffected;
+    }
+
+    @Override
+    public int delete(List<QueryTerm> queryTermList)
+    {
+        String queryTemplate = QueryStringBuilder.generateDeleteString(Term.TABLE_NAME, queryTermList);
+
+        List<Object> objectList = new ArrayList<Object>();
+
+        for(QueryTerm queryTerm : queryTermList)
+        {
+            objectList.add(queryTerm.getValue());
+        }
+
+        Object[] parameters = objectList.toArray();
+
+        int rowsAffected = jdbcTemplate.update(queryTemplate, parameters);
+
+        return rowsAffected;
+    }
+
+    private void addParameterMapValue(MapSqlParameterSource parameters, String insertColumnName, Term termModel)
+    {
+        String parameterName = QueryStringBuilder.convertColumnName(insertColumnName, false);
+
+        // Wish this could generalize
+        // The getter must be distinguished unless the models are designed as simply a map of columns-values
+        // Would prefer not being that generic since it may end up leading to all code being collections of strings
+
+        if(insertColumnName.equals(Term.getColumnName(Term.Columns.ID)))
+        {
+            parameters.addValue(parameterName, termModel.getId());
+        }
+        else if(insertColumnName.equals(Term.getColumnName(Term.Columns.TERM_NAME)))
+        {
+            parameters.addValue(parameterName, termModel.getTermName());
+        }
+
+        else if(insertColumnName.equals(Term.getColumnName(Term.Columns.CREATED_AT)))
+        {
+            parameters.addValue(parameterName, termModel.getCreatedAt());
+        }
+        else if(insertColumnName.equals(Term.getColumnName(Term.Columns.UPDATED_AT)))
+        {
+            parameters.addValue(parameterName, termModel.getUpdatedAt());
+        }
+        else
+        {
+            // should never end up here
+            // lists should have already been validated
+            throw new IllegalArgumentException("Invalid column name provided: " + insertColumnName);
+        }
+    }
+
+    private void addObjectValue(Map<String, Object> keyMap, String keyHolderColumnName, Term termModel)
+    {
+        if(keyHolderColumnName.equals(Term.getColumnName(Term.Columns.ID)))
+        {
+            termModel.setId((Integer) keyMap.get(keyHolderColumnName));
+        }
+        else if(keyHolderColumnName.equals(Term.getColumnName(Term.Columns.TERM_NAME)))
+        {
+            termModel.setTermName((String) keyMap.get(keyHolderColumnName));
+        }
+        else if(keyHolderColumnName.equals(Term.getColumnName(Term.Columns.CREATED_AT)))
+        {
+            termModel.setCreatedAt((Timestamp) keyMap.get(keyHolderColumnName));
+        }
+        else if(keyHolderColumnName.equals(Term.getColumnName(Term.Columns.UPDATED_AT)))
+        {
+            termModel.setUpdatedAt((Timestamp) keyMap.get(keyHolderColumnName));
+        }
+        else
+        {
+            // should never end up here
+            // lists should have already been validated
+            throw new IllegalArgumentException("Invalid column name provided: " + keyHolderColumnName);
+        }
+    }
+
+    @Override
+    public void validateColumnNames(List<String> columnNameList)
+    {
+        List<String> actualColumnNames = Term.getColumnNameList();
+        boolean valid = actualColumnNames.containsAll(columnNameList);
+
+        if(!valid)
+        {
+            List<String> invalidColumnNames = new ArrayList<>(columnNameList);
+            invalidColumnNames.removeAll(actualColumnNames);
+
+            throw new IllegalArgumentException("Invalid column names provided: " + invalidColumnNames);
+        }
+    }
 }
