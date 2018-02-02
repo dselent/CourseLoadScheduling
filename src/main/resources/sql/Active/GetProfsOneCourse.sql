cc57eba68e2b4926492ae9790b66726d22ed74db
NathanSiegel <siegelnathan08@gmail.com>
2018-02-02 18:52:59 -0500

    more sql updates

--- src/main/resources/sql/Active/GetProfsOneCourse.sql
+++ src/main/resources/sql/Active/GetProfsOneCourse.sql
@@ -2,5 +2,5 @@
 SELECT users.* FROM users WHERE id = 
 (SELECT users_id FROM faculty WHERE id = 
 (SELECT faculty_id FROM faculty_courses WHERE course_sections_id = :courseSectionsId))
-ORDER BY users.id; 
+ORDER BY users.id ASC; 
 
