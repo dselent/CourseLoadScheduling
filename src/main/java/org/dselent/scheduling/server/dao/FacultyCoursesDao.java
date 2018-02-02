2d891f80503245bd98738d89f75d5069c5ce74d6
NathanSiegel <siegelnathan08@gmail.com>
2018-02-02 12:46:05 -0500

    FacultyCourses Dao added

---
+++ src/main/java/org/dselent/scheduling/server/dao/FacultyCoursesDao.java
@@ -0,0 +1,13 @@
+package org.dselent.scheduling.server.dao;
+
+import org.dselent.scheduling.server.model.FacultyCourse;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Nathan on 2/2/2018.
+ */
+
+@Repository
+public interface FacultyCoursesDao extends Dao<FacultyCourse>{
+    
+}
