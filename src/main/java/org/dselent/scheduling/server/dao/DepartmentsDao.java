57fa1058361d46b620b01367bae2dbcfdd09f773
Robaire <robaire@galliath.com>
2018-02-02 13:16:22 -0500

    Removed Unecessary Imports

--- src/main/java/org/dselent/scheduling/server/dao/DepartmentsDao.java
+++ src/main/java/org/dselent/scheduling/server/dao/DepartmentsDao.java
@@ -1,7 +1,6 @@
 package org.dselent.scheduling.server.dao;
 
 
-import org.dselent.scheduling.server.dao.Dao;
 import org.dselent.scheduling.server.model.Department;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/org/dselent/scheduling/server/dao/FacultyDao.java
+++ src/main/java/org/dselent/scheduling/server/dao/FacultyDao.java
@@ -1,6 +1,5 @@
 package org.dselent.scheduling.server.dao;
 
-import org.dselent.scheduling.server.dao.Dao;
 import org.dselent.scheduling.server.model.Faculty;
 import org.springframework.stereotype.Repository;
 
