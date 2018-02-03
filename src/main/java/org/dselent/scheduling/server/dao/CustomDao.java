44217477346eb704e6135a910235e5e3254e8b66
Michael Capobianco <mccapobianco@wpi.edu>
2018-02-02 19:31:02 -0500

    added customDao's

--- src/main/java/org/dselent/scheduling/server/dao/CustomDao.java
+++ src/main/java/org/dselent/scheduling/server/dao/CustomDao.java
@@ -3,6 +3,14 @@ package org.dselent.scheduling.server.dao;
 import java.util.List;
 
 import org.dselent.scheduling.server.model.User;
+import org.dselent.scheduling.server.model.Course;
+import org.dselent.scheduling.server.model.Department;
+import org.dselent.scheduling.server.model.Faculty;
+import org.dselent.scheduling.server.model.CourseRequest;
+
+import org.dselent.scheduling.server.model.custommodel.GetAllCourses;
+import org.dselent.scheduling.server.model.custommodel.GetAllProfsDepts;
+import org.dselent.scheduling.server.model.custommodel.GetAllRequests;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,6 +23,11 @@ import org.springframework.stereotype.Repository;
 public interface CustomDao
 {
 	// custom queries here
-	
-	public List<User> getAllUsersWithRole(int roleId);
+	public List<User> customUsersWithRole(int roleId);
+	public List<GetAllCourses> getAllCourses(int facultyId);
+	public List<GetAllProfsDepts> getAllProfsDepts();
+	public List<GetAllRequests> getAllRequests();
+	public List<Course> getCourseOneProf(int facultyId);
+	public List<Department> getDeptOneProf(int facultyId);
+	public List<User>  getProfsOneCourse(int courseSectionId)
 }
