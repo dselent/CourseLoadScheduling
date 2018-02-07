f5e6f35de39e5bf55f84b999589e6e5df6fc193f
Robaire <robaire@galliath.com>
2018-02-06 21:54:13 -0500

    Created ScheduleController

---
+++ src/main/java/org/dselent/scheduling/server/controller/ScheduleController.java
@@ -0,0 +1,16 @@
+package org.dselent.scheduling.server.controller;
+
+import java.util.Map;
+
+import org.dselent.scheduling.server.requests.Register;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/schedule")
+public interface ScheduleController {
+
+    @RequestMapping(method=RequestMethod.POST, value=Register.REQUEST_NAME)
+    public ResponseEntity<String> getSchedules(@RequestBody Map<String, String> request) throws Exception;
+}
