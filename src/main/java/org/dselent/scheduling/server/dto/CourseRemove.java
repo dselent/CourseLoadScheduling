6c5ab3f4c5d912920628dfc2d56d0a08ef6f6eae
FabiGazi <fgaziano98@gmail.com>
2018-02-08 18:13:00 -0500

    CourseRemoveDto compleate

--- src/main/java/org/dselent/scheduling/server/dto/CourseRemove.java
+++
@@ -1,4 +0,0 @@
-package org.dselent.scheduling.server.dto;
-
-public class CourseRemove {
-}
--- src/main/java/org/dselent/scheduling/server/dto/CourseRemoveDto.java
+++ src/main/java/org/dselent/scheduling/server/dto/CourseRemoveDto.java
@@ -1,7 +1,127 @@
 package org.dselent.scheduling.server.dto;
 
+
+import javax.annotation.Generated;
+
 /**
- * Created by Nathan on 2/8/2018.
+ * Fabian Gaziano
  */
-public class CourseRemoveDto {
+
+
+public class CourseRemoveDto
+{
+    private final Integer courseId;
+
+
+    // I added to the auto-generated code
+    @Generated("SparkTools")
+    private CourseRemoveDto(Builder builder)
+    {
+        // can add defaults if null for other places where the builder pattern is used
+
+        this.courseId = builder.courseId;
+
+
+        // making claim that none of these can be null
+        // add other state checks here as necessary
+
+        if(this.courseId == null)
+        {
+            throw new IllegalStateException("Course Remove cannot be null");
+        }
+
+    }
+
+    public Integer getCourseId()
+    {
+        return courseId;
+    }
+
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((courseId == null) ? 0 : courseId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (!(obj instanceof CourseRemoveDto))
+        {
+            return false;
+        }
+        CourseRemoveDto other = (CourseRemoveDto) obj;
+
+        if (courseId == null)
+        {
+            if (other.courseId != null)
+            {
+                return false;
+            }
+        }
+        else if (!courseId.equals(other.courseId))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseRemoveDto{" +
+                "courseId=" + courseId +
+                '}';
+    }
+
+
+
+    /**
+     * Creates builder to build {@link CourseRemoveDto}.
+     * @return created builder
+     */
+    @Generated("SparkTools")
+    public static Builder builder()
+    {
+        return new Builder();
+    }
+
+    /**
+     * Builder to build {@link CourseRemoveDto}.
+     */
+    @Generated("SparkTools")
+    public static final class Builder
+    {
+        private Integer courseId;
+
+
+        private Builder()
+        {
+        }
+
+        public Builder withCourseId (Integer courseId)
+        {
+            this.courseId = courseId;
+            return this;
+        }
+
+
+
+        public CourseRemoveDto build()
+        {
+            return new CourseRemoveDto(this);
+        }
+    }
 }
