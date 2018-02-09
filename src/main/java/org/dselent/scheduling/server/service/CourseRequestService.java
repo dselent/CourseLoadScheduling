490f73fd50e4c8e6d36154172bddd199de982828
Michael Capobianco <mccapobianco@wpi.edu>
2018-02-09 18:13:09 -0500

    created courseRequestService and impl

---
+++ src/main/java/org/dselent/scheduling/server/service/CourseRequestService.java
@@ -0,0 +1,6 @@
+package org.dselent.scheduling.server.service;
+
+public interface CourseRequestService
+{
+    //move requests code from faculty to here
+}
---
+++ src/main/java/org/dselent/scheduling/server/service/impl/CourseRequestServiceImpl.java
@@ -0,0 +1,6 @@
+package org.dselent.scheduling.server.service.impl;
+
+public class CourseRequestServiceImpl
+{
+    //move request code from faculty to here
+}
