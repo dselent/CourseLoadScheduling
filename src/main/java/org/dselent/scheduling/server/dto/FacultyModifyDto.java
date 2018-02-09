fd763873c9aa80cb29f4f8d825c811df7c5c61a2
Krishna Madhurkar <krishnamadhurkar10@gmail.com>
2018-02-09 00:22:31 -0500

    Finished FacultyModifyDto.java

--- src/main/java/org/dselent/scheduling/server/dto/FacultyModifyDto.java
+++ src/main/java/org/dselent/scheduling/server/dto/FacultyModifyDto.java
@@ -1,4 +1,144 @@
 package org.dselent.scheduling.server.dto;
 
-public class FacultyModifyDto {
+import javax.annotation.Generated;
+import java.util.Objects;
+
+/**
+ * DTO = Data Transfer Object
+ * Used to package/wrap several variables into a single object
+ * Uses the Builder pattern for object instantiation
+ *
+ * @author dselent
+ *
+ */
+public class FacultyModifyDto
+{
+    private final Integer UserId;
+    private final String FacultyId;
+    private final String RequiredCredits;
+
+
+
+    // I added to the auto-generated code
+    @Generated("SparkTools")
+    private FacultyModifyDto(Builder builder)
+    {
+        // can add defaults if null for other places where the builder pattern is used
+
+        this.UserId = builder.UserId;
+        this.FacultyId = builder.FacultyId;
+        this.RequiredCredits = builder.RequiredCredits;
+
+
+
+        // making claim that none of these can be null
+        // add other state checks here as necessary
+
+        if(this.UserId == null)
+        {
+            throw new IllegalStateException("UserId cannot be null");
+        }
+        else if(this.FacultyId == null)
+        {
+            throw new IllegalStateException("FacultyId cannot be null");
+        }
+        else if(this.RequiredCredits == null)
+        {
+            throw new IllegalStateException("RequiredCredits cannot be null");
+        }
+
+
+    }
+
+    public Integer getUserId()
+    {
+        return UserId;
+    }
+
+    public String getFacultyId()
+    {
+        return FacultyId;
+    }
+
+    public String getRequiredCredits()
+    {
+        return RequiredCredits;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FacultyModifyDto that = (FacultyModifyDto) o;
+        return Objects.equals(UserId, that.UserId) &&
+                Objects.equals(FacultyId, that.FacultyId) &&
+                Objects.equals(RequiredCredits, that.RequiredCredits);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(UserId, FacultyId, RequiredCredits);
+    }
+
+    @Override
+    public String toString() {
+        return "FacultyModifyDto{" +
+                "UserId=" + UserId +
+                ", FacultyId='" + FacultyId + '\'' +
+                ", RequiredCredits='" + RequiredCredits + '\'' +
+                '}';
+    }
+
+    /**
+     * Creates builder to build {@link FacultyModifyDto}.
+     * @return created builder
+     */
+    @Generated("SparkTools")
+    public static Builder builder()
+    {
+        return new Builder();
+    }
+
+    /**
+     * Builder to build {@link FacultyModifyDto}.
+     */
+    @Generated("SparkTools")
+    public static final class Builder
+    {
+        private Integer UserId;
+        private String FacultyId;
+        private String RequiredCredits;
+
+
+
+        private Builder()
+        {
+        }
+
+        public Builder withUserId(Integer UserId)
+        {
+            this.UserId = UserId;
+            return this;
+        }
+
+        public Builder withFacultyId(String FacultyId)
+        {
+            this.FacultyId = FacultyId;
+            return this;
+        }
+
+        public Builder withRequiredCredits(String RequiredCredits)
+        {
+            this.RequiredCredits = RequiredCredits;
+            return this;
+        }
+
+
+        public FacultyModifyDto build()
+        {
+            return new FacultyModifyDto(this);
+        }
+    }
 }
