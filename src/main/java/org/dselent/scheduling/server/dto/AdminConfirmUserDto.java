69ec0c31d8bc6ebfb5dbbf7ba0d3900ad3b2e029
Krishna Madhurkar <krishnamadhurkar10@gmail.com>
2018-02-09 03:18:36 -0500

    Started AdminConfirmUserDto.java

---
+++ src/main/java/org/dselent/scheduling/server/dto/AdminConfirmUserDto.java
@@ -0,0 +1,17 @@
+package org.dselent.scheduling.server.dto;
+
+import javax.annotation.Generated;
+import java.util.Objects;
+
+/**
+ * DTO = Data Transfer Object
+ * Used to package/wrap several variables into a single object
+ * Uses the Builder pattern for object instantiation
+ *
+ * @author dselent
+ *
+ */
+public class AdminConfirmUserDto
+{
+   
+}
