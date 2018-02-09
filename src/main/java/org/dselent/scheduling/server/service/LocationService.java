ebf5063206111b42acb2626d4094a61af7721c46
Krishna Madhurkar <krishnamadhurkar10@gmail.com>
2018-02-09 15:31:28 -0500

    Finished LocationService.java

--- src/main/java/org/dselent/scheduling/server/service/LocationService.java
+++ src/main/java/org/dselent/scheduling/server/service/LocationService.java
@@ -1,4 +1,19 @@
 package org.dselent.scheduling.server.service;
 
-public interface LocationService {
+import java.sql.SQLException;
+import java.util.List;
+
+import org.dselent.scheduling.server.dto.LocationAddDto;
+import org.dselent.scheduling.server.dto.LocationModifyDto;
+import org.dselent.scheduling.server.dto.LocationRemoveDto;
+import org.dselent.scheduling.server.model.Location;
+import org.dselent.scheduling.server.requests.LocationRemove;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface LocationService
+{
+    public List<Integer> addLocation(LocationAddDto LocationAddDto);
+    public List<Integer> modifyLocation(LocationModifyDto LocationModifyDto);
+    public List<Integer> removeLocation(LocationRemoveDto LocationRemoveDto);
 }
