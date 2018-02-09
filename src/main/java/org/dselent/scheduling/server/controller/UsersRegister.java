cf62a11f4f22740541eed49ec8123678f4ba9a73
Robaire <robaire@galliath.com>
2018-02-08 23:20:13 -0500

    Created UsersLogin controller

--- src/main/java/org/dselent/scheduling/server/controller/UsersRegister.java
+++ src/main/java/org/dselent/scheduling/server/controller/UsersRegister.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/user/register")
-public interface UsersController
+@RequestMapping("/users/register")
+public interface UserRegister
 {
     
-    @RequestMapping(method=RequestMethod.POST, value=UserRegister.REQUEST_NAME)
+    @RequestMapping(method=RequestMethod.POST, value= org.dselent.scheduling.server.requests.UserRegister.REQUEST_NAME)
 
     // Registers a user in the system
 	public ResponseEntity<String> register(@RequestBody Map<String, String> request) throws Exception;
