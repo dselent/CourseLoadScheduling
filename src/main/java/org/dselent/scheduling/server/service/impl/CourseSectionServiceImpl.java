d6c86865fa64ee9345b5dbb27bdcdefc91eda889
NathanSiegel <siegelnathan08@gmail.com>
2018-02-09 14:46:37 -0500

    actually finished addCourseSection in CourseSectionsImpl

--- src/main/java/org/dselent/scheduling/server/service/impl/CourseSectionServiceImpl.java
+++ src/main/java/org/dselent/scheduling/server/service/impl/CourseSectionServiceImpl.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.dselent.scheduling.server.dao.CourseSectionsDao;
+import org.dselent.scheduling.server.dao.CourseSectionsTermsDao;
+import org.dselent.scheduling.server.dao.TermsDao;
 import org.dselent.scheduling.server.dto.CourseSectionAddDto;
 import org.dselent.scheduling.server.dto.CourseSectionModifyDto;
 import org.dselent.scheduling.server.dto.CourseSectionRemoveDto;
 import org.dselent.scheduling.server.model.CourseSection;
+import org.dselent.scheduling.server.model.CourseSectionTerm;
+import org.dselent.scheduling.server.model.Term;
 import org.dselent.scheduling.server.service.CourseSectionService;
 
 import org.dselent.scheduling.server.sqlutils.ComparisonOperator;
+import org.dselent.scheduling.server.sqlutils.LogicalOperator;
 import org.dselent.scheduling.server.sqlutils.QueryTerm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +32,8 @@ import static org.dselent.scheduling.server.sqlutils.ComparisonOperator.EQUAL;
 public class CourseSectionServiceImpl implements CourseSectionService{
     @Autowired
     private CourseSectionsDao courseSectionsDao;
+    private TermsDao termsDao;
+    private CourseSectionsTermsDao courseSectionsTermsDao;
 
     public CourseSectionServiceImpl(){
 
@@ -43,22 +50,72 @@ public class CourseSectionServiceImpl implements CourseSectionService{
         CourseSection courseSection = new CourseSection();
         courseSection.setCoursesId(dto.getCourseId());
         courseSection.setSectionType(dto.getSectionType());
+        
+        String termName = dto.getTerm();
 
-        /*need to figure out how to do this as well */
-        String term = dto.getTerm();
+        List<String> courseSectionInsertColumnNameList = new ArrayList<>();
+        List<String> courseSectionKeyHolderColumnNameList = new ArrayList<>();
 
-        List<String> courseInsertColumnNameList = new ArrayList<>();
-        List<String> courseKeyHolderColumnNameList = new ArrayList<>();
+        courseSectionInsertColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.COURSES_ID));
+        courseSectionInsertColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.SECTION_TYPE));
 
-        courseInsertColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.COURSES_ID));
-        courseInsertColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.SECTION_TYPE));
+        courseSectionKeyHolderColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.ID));
+        courseSectionKeyHolderColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.CREATED_AT));
+        courseSectionKeyHolderColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.UPDATED_AT));
 
-        courseKeyHolderColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.ID));
-        courseKeyHolderColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.CREATED_AT));
-        courseKeyHolderColumnNameList.add(CourseSection.getColumnName(CourseSection.Columns.UPDATED_AT));
+        rowsAffectedList.add(courseSectionsDao.insert(courseSection, courseSectionInsertColumnNameList, courseSectionKeyHolderColumnNameList));
 
+        //adding the link to the course's term
 
-        rowsAffectedList.add(courseSectionsDao.insert(courseSection, courseInsertColumnNameList, courseKeyHolderColumnNameList));
+        //select CourseSectionId
+        List<String> courseSectionIdEntry = new ArrayList<>();
+        List<QueryTerm> courseSectionQueryTermList = new ArrayList<>();
+
+        courseSectionIdEntry.add(CourseSection.getColumnName(CourseSection.Columns.ID));
+
+        courseSectionQueryTermList.add(new QueryTerm(CourseSection.getColumnName(CourseSection.Columns.COURSES_ID),EQUAL,courseSection.getCoursesId(), LogicalOperator.AND));
+        courseSectionQueryTermList.add(new QueryTerm(CourseSection.getColumnName(CourseSection.Columns.SECTION_TYPE),EQUAL,courseSection.getSectionType(), null));
+
+        List<CourseSection> courseSectionsList = courseSectionsDao.select(courseSectionIdEntry, courseSectionQueryTermList, null);
+
+        if(courseSectionsList.isEmpty()){
+            return null;
+        }
+
+        Integer courseSectionId = courseSectionsList.get(0).getId();
+
+        //select TermId
+        List<String> termIdEntry = new ArrayList<>();
+        List<QueryTerm> termQueryTermList = new ArrayList<>();
+
+        termIdEntry.add(Term.getColumnName(Term.Columns.ID));
+
+        termQueryTermList.add(new QueryTerm(Term.getColumnName(Term.Columns.TERM_NAME),EQUAL,termName,null));
+
+        List<Term> termsList = termsDao.select(termIdEntry,termQueryTermList,null);
+
+        if(termsList.isEmpty()){
+            return null;
+        }
+
+        Integer termId = termsList.get(0).getId();
+
+        //add CourseSectionsTerms entry
+        CourseSectionTerm courseSectionTerm = new CourseSectionTerm();
+        courseSectionTerm.setCourseSectionsId(courseSectionId);
+        courseSectionTerm.setTermsId(termId);
+
+        List<String> courseSectionTermInsertColumnNameList = new ArrayList<>();
+        List<String> courseSectionTermKeyHolderColumnNameList = new ArrayList<>();
+
+        courseSectionTermInsertColumnNameList.add(CourseSectionTerm.getColumnName(CourseSectionTerm.Columns.COURSE_SECTIONS_ID));
+        courseSectionTermInsertColumnNameList.add(CourseSectionTerm.getColumnName(CourseSectionTerm.Columns.TERMS_ID));
+
+        courseSectionTermKeyHolderColumnNameList.add(CourseSectionTerm.getColumnName(CourseSectionTerm.Columns.ID));
+        courseSectionTermKeyHolderColumnNameList.add(CourseSectionTerm.getColumnName(CourseSectionTerm.Columns.CREATED_AT));
+        courseSectionTermKeyHolderColumnNameList.add(CourseSectionTerm.getColumnName(CourseSectionTerm.Columns.UPDATED_AT));
+
+        rowsAffectedList.add(courseSectionsTermsDao.insert(courseSectionTerm,courseSectionInsertColumnNameList,courseSectionKeyHolderColumnNameList));
 
         return rowsAffectedList;
     }
