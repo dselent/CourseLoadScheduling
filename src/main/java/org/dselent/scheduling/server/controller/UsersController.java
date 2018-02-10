2dba7f1863f601eb0b5a99ef128b0328cce558c4
NathanSiegel <siegelnathan08@gmail.com>
2018-02-09 19:24:37 -0500

    finished UsersController interface

--- src/main/java/org/dselent/scheduling/server/controller/UsersController.java
+++ src/main/java/org/dselent/scheduling/server/controller/UsersController.java
@@ -3,6 +3,8 @@ package org.dselent.scheduling.server.controller;
 import java.util.Map;
 
 import org.dselent.scheduling.server.requests.UserLogin;
+import org.dselent.scheduling.server.requests.UserLogout;
+import org.dselent.scheduling.server.requests.UserModify;
 import org.dselent.scheduling.server.requests.UserRegister;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +15,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface UsersController
 {
     // Registers a user in the system
-    @RequestMapping(method=RequestMethod.POST, value= org.dselent.scheduling.server.requests.UserRegister.REQUEST_NAME)
+    @RequestMapping(method=RequestMethod.POST, value= UserRegister.REQUEST_NAME)
 	public ResponseEntity<String> register(@RequestBody Map<String, String> request) throws Exception;
 
     // Logs a User into the system
     @RequestMapping(method=RequestMethod.POST, value= UserLogin.REQUEST_NAME)
     public ResponseEntity<String> login(@RequestBody Map<String, String> request) throws Exception;
+
+    //Logs a User out of the system
+    @RequestMapping(method=RequestMethod.POST, value= UserLogout.REQUEST_NAME)
+    public ResponseEntity<String> logout(@RequestBody Map<String,String> request) throws Exception;
+
+    //Modifies a User
+    @RequestMapping(method=RequestMethod.POST, value= UserModify.REQUEST_NAME)
+    public ResponseEntity<String> modify(@RequestBody Map<String, String> request) throws Exception;
+    
 }
 
 	
