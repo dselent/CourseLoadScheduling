bd0c82ee68058c73b1c470095c3abeb64deea144
dselent <selentd@gmail.com>
2018-02-12 12:57:46 -0500

    Fixed some incorrect URI paths in the controllers

--- src/main/java/org/dselent/scheduling/server/controller/CourseRequestsController.java
+++ src/main/java/org/dselent/scheduling/server/controller/CourseRequestsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/courseDepartments")
+@RequestMapping("/courseRequests")
 public interface CourseRequestsController
 {
     // Registers a user in the system
--- src/main/java/org/dselent/scheduling/server/controller/CoursesController.java
+++ src/main/java/org/dselent/scheduling/server/controller/CoursesController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/term")
+@RequestMapping("/course")
 public interface CoursesController
 {
     // Adds a term in the system
